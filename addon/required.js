f3dbd35dec9daafa57d3133c0ba86cc30748d42f
John Huynh <john@huynh.io>
2015-08-22 21:36:08 -0400

    Add type check for string and number.

--- addon/required.js
+++ addon/required.js
@@ -1,5 +1,20 @@
 import Ember from 'ember';
 
+const _supportedAttrTypes = {
+  'bool': () => {
+  },
+  'function': () => {
+  },
+  'number': (value) => {
+    return Ember.isPresent(value) && typeof value === 'number';
+  },
+  'object': () => {
+  },
+  'string': (value) => {
+    return Ember.isPresent(value) && typeof value === 'string';
+  },
+};
+
 function _defaultAssertMessage(attrName, attrType) {
   if (Ember.isPresent(attrType)) {
     return ['Component', attrName, 'of type', attrType, 'is required'].join(' ');
@@ -21,13 +36,21 @@ function _enforceAttrMixin(attrName, errorMessage) {
   });
 }
 
-// function _enforceAttrWithTypeMixin(attrName, attrType, errorMessage) {
-//   return Ember.Mixin.create({
-//     init() {
-//       this._super(...arguments);
-//     }
-//   });
-// }
+function _enforceAttrWithTypeMixin(attrName, attrType, errorMessage) {
+  return Ember.Mixin.create({
+    init() {
+      this._super(...arguments);
+
+      if (Ember.isEmpty(errorMessage)) {
+        errorMessage = _defaultAssertMessage(attrName, attrType);
+      }
+
+      const attrs = this.get('attrs');
+      Ember.assert(errorMessage, attrs.hasOwnProperty(attrName));
+      Ember.assert(errorMessage, _supportedAttrTypes[attrType](attrs[attrName]));
+    }
+  });
+}
 
 export default function required(attrName, options) {
   Ember.assert('Enforcer.required() requires an attrName as the first argument',
@@ -42,11 +65,17 @@ export default function required(attrName, options) {
   }
 
   let mixin;
-  // if (Ember.isPresent(type)) {
-  //   mixin = enforceAttrWithTypeMixin(attrName, type, message)
-  // } else {
+  if (Ember.isPresent(type)) {
+    let errorMessage = 'Enforcer.required() only accepts ' +
+      'options.type of the following values: ' +
+      Object.keys(_supportedAttrTypes).join(', ');
+
+    Ember.assert(errorMessage, _supportedAttrTypes.hasOwnProperty(type));
+
+    mixin = _enforceAttrWithTypeMixin(attrName, type, message);
+  } else {
     mixin = _enforceAttrMixin(attrName, message);
-  // }
+  }
 
   return mixin;
 }
--- tests/unit/required-test.js
+++ tests/unit/required-test.js
@@ -20,38 +20,164 @@ test('throws when attrName argument is missing', (assert) => {
   assert.throws(errorBlock);
 });
 
-test('can be mixed into a component', (assert) => {
-    const klass = Ember.Component.extend(Enforcer.required('someAttr'), {});
-    const component = klass.create({
-      attrs: { 'someAttr': true }
-    });
+test('throws when options.type is not a supported type', (assert) => {
+  const errorBlock = () => {
+    Enforcer.required('someAttr', { type: 'apples' });
+  };
 
-    assert.ok(component);
+  assert.throws(errorBlock);
 });
 
-test('throws when mixed in component is missing attr', (assert) => {
+test('can be mixed into a component', (assert) => {
     const klass1 = Ember.Component.extend(Enforcer.required('someAttr'), {});
+    const component1 = klass1.create({
+      attrs: { 'someAttr': true }
+    });
+    assert.ok(component1, 'Can create component that requires attr');
+
+    const klass2 = Ember.Component.extend(
+      Enforcer.required('someAttr', {
+        type: 'string'
+      }), {
+    });
+    const component2 = klass2.create({
+      attrs: { 'someAttr': 'yas' }
+    });
+    assert.ok(component2, 'Can create component that requires attr of specific type');
+});
+
+test('throws default error messages', (assert) => {
+    const klass1 = Ember.Component.extend(
+      Enforcer.required('someAttr'), {
+    });
     const errorBlock1 = () => {
-      const component = klass1.create({
+      klass1.create({
         attrs: {}
       });
     };
 
     assert.throws(errorBlock1, (error) => {
       return Ember.isPresent(error.message.match(/someAttr/));
-    }, 'throws default error message');
+    }, 'throws default error message for attr check');
 
     const klass2 = Ember.Component.extend(
-      Enforcer.required('someAttr', { message: 'MAGIC' }),
-      {});
+      Enforcer.required('someAttr', { type: 'string' }), {
+    });
     const errorBlock2 = () => {
-      const component = klass2.create({
+      klass2.create({
+        attrs: { 'someAttr': true }
+      });
+    };
+
+    assert.throws(errorBlock2, (error) => {
+      return Ember.isPresent(error.message.match(/someAttr/));
+    }, 'throws default error message for attr and type check');
+});
+
+test('throws custom error messages', (assert) => {
+    const klass1 = Ember.Component.extend(
+      Enforcer.required('someAttr', { message: 'MAGIC' }), {
+    });
+    const errorBlock1 = () => {
+      klass1.create({
         attrs: {}
+      });
+    };
+
+    assert.throws(errorBlock1, (error) => {
+      return Ember.isPresent(error.message.match(/MAGIC/));
+    }, 'throws custom error message for attr check');
+
+    const klass2 = Ember.Component.extend(
+      Enforcer.required('someAttr', {
+        type: 'string',
+        message: 'MAGIC',
+      }), {
+    });
+    const errorBlock2 = () => {
+      klass2.create({
+        attrs: { 'someAttr': true }
       });
     };
 
     assert.throws(errorBlock2, (error) => {
       return Ember.isPresent(error.message.match(/MAGIC/));
-    }, 'throws custom error message');
+    }, 'throws custom error message for attr and type check');
+});
+
+test('throws when mixed in component is missing attr', (assert) => {
+    const klass1 = Ember.Component.extend(Enforcer.required('someAttr'), {});
+    const errorBlock1 = () => {
+      klass1.create({
+        attrs: {}
+      });
+    };
+
+    assert.throws(errorBlock1, 'throws when attr is specified');
+
+    const klass2 = Ember.Component.extend(
+      Enforcer.required('someAttr', { type: 'string' }),
+      {});
+    const errorBlock2 = () => {
+      klass2.create({
+        attrs: {}
+      });
+    };
+
+    assert.throws(errorBlock2, 'throws when attr and type is specified');
+});
+
+test('checks string attr type', (assert) => {
+    const klass = Ember.Component.extend(
+      Enforcer.required('someAttr', {
+        type: 'string'
+      }), {
+    });
+
+    const errorBlock1 = () => {
+      klass.create({
+        attrs: {}
+      });
+    };
+    assert.throws(errorBlock1, 'throws when missing attr');
+
+    const errorBlock2 = () => {
+      klass.create({
+        attrs: { 'someAttr': true }
+      });
+    };
+    assert.throws(errorBlock2, 'throws when attr is incorrect type');
+
+    const component = klass.create({
+      attrs: { 'someAttr': 'yas' }
+    });
+    assert.ok(component, 'can create with valid attr and type');
+});
+
+test('checks number attr type', (assert) => {
+    const klass = Ember.Component.extend(
+      Enforcer.required('someAttr', {
+        type: 'number'
+      }), {
+    });
+
+    const errorBlock1 = () => {
+      klass.create({
+        attrs: {}
+      });
+    };
+    assert.throws(errorBlock1, 'throws when missing attr');
+
+    const errorBlock2 = () => {
+      klass.create({
+        attrs: { 'someAttr': true }
+      });
+    };
+    assert.throws(errorBlock2, 'throws when attr is incorrect type');
+
+    const component = klass.create({
+      attrs: { 'someAttr': 124 }
+    });
+    assert.ok(component, 'can create with valid attr and type');
 });
 
